fa5a222111fea98a302fe0e3a8c54e30f2b34a4f
Felipe Beserra <49963588+Beserrovsky@users.noreply.github.com>
2021-04-11 19:09:16 -0300

    Basic setup of dependencies, env and workflow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
 
 function App() {
+
+  console.log(process.env.REACT_APP_API_URL);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <div className="body container">
+        
+      </div>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            Salve
+        </nav>
+    );
+}
+ 
+export default Navbar;
