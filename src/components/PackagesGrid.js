1d555dc16b54961b281ec565909763791d805b03
felipebeserra25@gmail.com <felipebeserra25@gmail.com>
2021-07-31 20:30:16 -0300

    Package line clamp

--- src/components/PackagesGrid.js
+++ src/components/PackagesGrid.js
@@ -11,8 +11,6 @@ const PackagesGrid = () => {
 
     const changeCardOpened = (index) => {
         setCardOpenedIndex(index);
-        console.log("Index changed to " + CardOpenedIndex );
-        console.log(data);
     }
 
     const badData = ()=>
@@ -57,7 +55,7 @@ const PackagesGrid = () => {
 
             <h1 className="text-center mb-5">Nossos pacotes!</h1>
 
-            <div className="row justify-content-center">
+            <div className="row justify-content-around">
                 {error && ''+error}
                 {loading && <div className="loader"></div>}
                 {Array.isArray(data) && data.map(p => (
@@ -74,7 +72,7 @@ const PackagePreview = ({packageReference, updateIndex, opened}) => {
     const IMAGE_URL = `${process.env.REACT_APP_API_URL}/api/Package/${packageReference.id}/image`;
 
     return (
-        <div className={`package-preview ${opened? 'col-8' : 'col-3'} mx-3 mb-5 pointer rounded p-0`} onClick={()=>{if(!opened) updateIndex(packageReference.id)}}>
+        <div className={`package-preview ${opened? 'col-6' : 'col-2'} mx-5 mb-5 pointer rounded p-0`} onClick={()=>{if(!opened) updateIndex(packageReference.id)}}>
 
             {!opened && // CLOSED 
             <div className={`package-preview-close w-100 h-100 d-flex align-items-end rounded shadow-sm`}style={{backgroundImage:`url(${IMAGE_URL})`}}>
@@ -84,18 +82,19 @@ const PackagePreview = ({packageReference, updateIndex, opened}) => {
 
             {opened && // OPENED
             <div className={`package-preview-open w-100 h-100 d-flex flex-column justify-content-between shadow rounded`}>
-                <div className="image-preview h-100 d-flex align-items-end rounded-top" style={{backgroundImage:`url(${IMAGE_URL})`}}>
+                <div className="image-preview h-100 d-flex align-items-end rounded-top pt-4" style={{backgroundImage:`url(${IMAGE_URL})`}}>
                     <h4 className="text-white text-uppercase pl-3 font-weight-bold">{packageReference.name}</h4>
                 </div>
-                <div className="text-left p-3">
-                    <h6>{packageReference.description}</h6>
-                    <div className="d-flex justify-content-between mt-3">
+                <div className="text-left px-3">
+                    <p className="mb-1 my-2 short-desc">{packageReference.description}</p>
+                    <div className="d-flex justify-content-between mt-1">
                         <div>
-                            <h6 className="font-weight-bold text-uppercase mb-2 mt-3">Preço</h6>
+                            <h6 className="font-weight-bold text-uppercase">Preço</h6>
                             <h6>{(packageReference.imageQuantity === null)? `R$ ${packageReference.baseValue.toFixed(2)} + ${packageReference.pricePerPhoto.toFixed(2)} p/Foto` : `R$ ${packageReference.baseValue.toFixed(2)}`}</h6>
                         </div>
-                        <div className="d-flex align-items-end">
-                            <a className="btn btn-primary" href="/">Ver mais</a>
+                        <div className="d-flex align-items-center">
+                            <a className="btn btn-info" href="/">Ver mais</a>
+                            <a className="btn btn-primary ml-3" href="/"><i class="bi bi-cart-plus"></i> Carrinho</a>
                         </div>
                     </div>
                 </div>
