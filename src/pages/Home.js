93f984d48a0efb58344dd9f5bc0b742ade3c9fa0
felipebeserra25@gmail.com <felipebeserra25@gmail.com>
2021-08-25 08:43:45 -0300

    Responsive publicity sections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,56 @@
 import Banner from "../components/Banner";
 import PackagesGrid from "../components/PackagesGrid";
+import { Link } from "react-router-dom";
+
+import photo from "../assets/svgs/photo.svg";
+import camera_photos from "../assets/svgs/camera_photos.svg";
+import woman_photoshoot from "../assets/svgs/woman_photoshoot.svg";
 
 const Home = () => {
     return ( 
         <div className="home">
             <Banner/>
-            <div className="container">
+            <div className="container pt-5">
+                <div className="row d-flex justify-content-center mt-5">
+                    <img src={camera_photos} className="d-lg-none col-6 mb-3" alt=""/>
+                    <div className="col-9 col-lg-9 d-flex align-items-center">
+                        <img src={camera_photos} className="w-50 pb-5 d-none d-lg-block" alt=""/>
+                        <div className="ml-lg-5">
+                            <h5 className="text-center text-lg-left">Lorem ipsum dolor sit amet, consectetur adipiscing 
+                            elit, sed do eiusmod tempor incididunt ut labore et 
+                            dolore magna aliqua</h5>
+                            <div className="d-flex justify-content-center justify-content-lg-start">
+                                <Link to="/" className="btn btn-outline-primary rounded mt-2 mt-lg-3">Exemplos</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row d-flex justify-content-center mt-5 mt-lg-3">
+                    <img src={photo} className="d-lg-none col-6 ml-5 mb-3" alt=""/>
+                    <div className="col-9 col-lg-9 d-flex align-items-center">
+                        <div className="mr-lg-5">
+                            <h5 className="text-center text-lg-right">Lorem ipsum dolor sit amet, consectetur adipiscing 
+                            elit, sed do eiusmod tempor incididunt ut labore et 
+                            dolore magna aliqua</h5>
+                            <div className="d-flex justify-content-center justify-content-lg-end">
+                                <Link to="/" className="btn btn-outline-primary rounded mt-2 mt-lg-3">Exemplos</Link>
+                            </div>
+                        </div>
+                        <img src={photo} className="w-75 pb-5 d-none d-lg-block" alt=""/>
+                    </div>
+                </div>
+                <div className="row d-flex justify-content-center mt-5 mt-lg-3">
+                    <img src={woman_photoshoot} className="d-lg-none col-6 mb-3" alt=""/>
+                    <div className="col-9 col-lg-9 d-flex align-items-center">
+                        <img src={woman_photoshoot} className="w-50 pb-5 d-none d-lg-block" alt=""/>
+                        <div className="ml-lg-5">
+                            <h5 className="text-center text-lg-left">Lorem ipsum dolor sit amet, consectetur adipiscing 
+                            elit, sed do eiusmod tempor incididunt ut labore et 
+                            dolore magna aliqua</h5>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="row pt-5">
                     <PackagesGrid/>
                 </div>
