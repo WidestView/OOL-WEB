b5b09046d3f363d24a4f60fba0e2714116357987
felipebeserra25@gmail.com <felipebeserra25@gmail.com>
2021-08-27 12:54:11 -0300

    Creature1 added

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,31 +1,38 @@
-// Environment
-import ground from "../assets/svgs/environment/ground.svg"; 
+import { Link } from "react-router-dom";
 
+// Environment
+import leaf from "../assets/svgs/environment/leaf.svg";
 
 // Creatures
-import creature1 from "../assets/svgs/creatures/creature1.svg"; 
+import creature1 from "../assets/svgs/creatures/creature1.svg";
+import creature1_arm from "../assets/svgs/creatures/creature1_arm.svg";
+
 
 const NotFound = () => {
     return ( 
-        <div className="not-found container-fluid bg-white">
+        <div className="not-found container bg-white mt-3">
             <div className="row">
-                <div className="col banner-animated">
-                    <img src={creature1} className="mt-2 mt-lg-6" id="ground" alt=""/>
-                    <img src={ground} className="w-100" id="ground" alt=""/>
-                </div>
-            </div>
-            <div className="row">
-                <div className="d-flex col-12 justify-content-center mt-5">
-                    <p className="display-1 text-primary font-weight-bold">404</p>
-                </div>
-                <div className="col-5 mx-auto mt-3">
-                    <div className="d-flex justify-content-center">
-                        <p className="text-center font-wheight-bold text-primary mb-0 pb-0">PROCURAMOS EM TODO CANTO</p>
-                        <p className="text-center text-dark mb-0 pb-0">,</p>
+                <div className="col-12 position-relative banner-animated">
+                    <div className="creature" id="creature1">
+                        <img src={creature1} id="creature1_body" alt=""/>
+                        <img src={creature1_arm} className="creature" id="creature1_arm" alt=""/>
                     </div>
-                    <p className="text-center text-dark mt-0 pt-0">mas essa página não foi encontrada</p>
+                    <img src={leaf} id="leaf" alt=""/>
+                    <div id="ground"/>
                 </div>
-                <a href="/" className="btn btn-primary rounded">Voltar</a>
+                <div className="col-12 banner-animated-body">
+                    <h1 className="display-1 text-primary text-center font-weight-bold">404</h1>
+                    <div className="col-5 mx-auto mt-3">
+                        <div className="d-flex justify-content-center">
+                            <p className="text-center font-weight-bold text-primary mb-0 pb-0">PROCURAMOS EM TODO CANTO</p>
+                            <p className="text-center text-dark mb-0 pb-0">,</p>
+                        </div>
+                        <p className="text-center text-dark mt-0 pt-0">mas essa página não foi encontrada</p>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <Link to="/" className="btn btn-primary rounded">Voltar</Link>
+                    </div>
+                </div>
             </div>
         </div>
      );
