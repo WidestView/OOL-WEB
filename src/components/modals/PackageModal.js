d4e6bafeda14f3f35fcd44c77bfd110fae633275
felipebeserra25@gmail.com <felipebeserra25@gmail.com>
2021-12-04 21:52:10 -0300

    Tamax Package stuff
    Co-authored-by: tamaxx <tamaxx@users.noreply.github.com>

--- src/components/modals/PackageModal.js
+++ src/components/modals/PackageModal.js
@@ -7,14 +7,17 @@ const PackageModal = ({pack}) => {
     return ( 
         <div className="modal fade" id="packageModal" tabIndex="-1" role="dialog" data-keyboard="false" data-backdrop="static">
             <div className="modal-dialog modal-dialog-centered" role="document">
-                <div className="modal-content">
+                <div className="modal-content text-titillium">
                     <div className="modal-header">
-                        <h4 className="modal-title">{Title}</h4>
+                        <h5 className="modal-title font-weight-bold">{Title}</h5>
                         <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     </div>
                     <div className="modal-body">
+                    <h5 className="font-weight-bold text-uppercase text-center my-2">{pack.name}</h5>
                         <img src={`${PackageAPI.getImageUrl(pack.id)}`} className="w-100" alt="" />
-                        <h1>{pack.name}</h1>
+                        <p className="mx-3 mt-4">{pack.description}</p>
+                        <p className="mx-3"><b>Valor final:</b> {pack.baseValue}</p>
+                        <button type="button" className="btn btn-primary mx-3 mb-2 float-right">Comprar</button>
                     </div>
                 </div>
             </div>
