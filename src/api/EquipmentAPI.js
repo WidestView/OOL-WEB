0d58cebdeec3148c1511400ebd26f70b99513a0e
felipebeserra25@gmail.com <felipebeserra25@gmail.com>
2021-12-14 13:02:22 -0300

    Equipment API CR[U]

--- src/api/EquipmentAPI.js
+++ src/api/EquipmentAPI.js
@@ -1,27 +1,78 @@
 import axios from "axios";
 import { isDev } from "../util/Env";
 
-class EquipmentAPI { //TODO: CRU EQUIPMENT
+class EquipmentAPI { //TODO: PUT EQUIPMENT
 
-    static get = async (id) => {
+    static get = async (id)=> {
+        let data;
+        // data = await EquipmentAPI.getDetails(id)
+        return data;
+    };
+
+    static post = async (data) => {
+        await EquipmentAPI.postDetails(data);
+    }
+
+    static getEquipment = async (id) => {
 
         if (id !== undefined) { 
-            // let res = await axios.get(`YET TO IMPLEMENT`);
-            // let employee = res.data;
-            // if(employee !== undefined && isDev()){
-            //     console.info("EMPLOYEE INFO:");
-            //     console.info(employee);
-            // }
-            // return employee;
+            let res = await axios.get(`${process.env.REACT_APP_API_URL}/api/Equipment/${id}`);
+            let equipment = res.data;
+            if(equipment !== undefined && isDev()){
+                console.info("EQUIPMENT INFO:");
+                console.info(equipment);
+            }
+            return equipment;
         }
 
-        // let res = await axios.get(`${process.env.REACT_APP_API_URL}/api/Employee`);
-        // let employees = res.data;
-        // if(employees !== undefined && isDev()){
-        //     console.info("EMPLOYEES INFO:");
-        //     console.info(employees);
-        // }
-        // return employees;
+        let res = await axios.get(`${process.env.REACT_APP_API_URL}/api/Equipment`);
+        let equipments = res.data;
+        if(equipments !== undefined && isDev()){
+            console.info("EQUIPMENTS INFO:");
+            console.info(equipments);
+        }
+        return equipments;
+    }
+
+    static postDetails = async (equipment) => {
+        let res = await axios.post(`${process.env.REACT_APP_API_URL}/api/Equipment`, equipment);
+        equipment = res.data;
+        if(equipment !== undefined && isDev()){
+            console.info("EQUIPMENT INFO:");
+            console.info(equipment);
+        }
+        return equipment;
+    }
+
+    static getDetails = async (id) => {
+
+        if (id !== undefined) { 
+            let res = await axios.get(`${process.env.REACT_APP_API_URL}/api/equipment/details/${id}`);
+            let details = res.data;
+            if(details !== undefined && isDev()){
+                console.info("EQUIPMENT DETAILS INFO:");
+                console.info(details);
+            }
+            return details;
+        }
+
+        let res = await axios.get(`${process.env.REACT_APP_API_URL}/api/equipment/details`);
+        let details = res.data;
+        if(details !== undefined && isDev()){
+            console.info("EQUIPMENTS DETAILS INFO:");
+            console.info(details);
+        }
+        return details;
+    }
+
+    static postDetails = async (details) => {
+        let res = await axios.post(`${process.env.REACT_APP_API_URL}/api/equipment/details`, details);
+        details = res.data;
+        if(details !== undefined && isDev()){
+            console.info("EQUIPMENT DETAILS INFO:");
+            console.info(details);
+        }
+        return details;
     }
 }
 
