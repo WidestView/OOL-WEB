9bcb3a83e8dbe2719a43233a52ffc0cf46d357ac
tamaxx <pedro.tamas@uol.com.br>
2021-12-14 12:51:23 -0300

    Package Order modal update

--- src/components/modals/PackageOrderModal.js
+++ src/components/modals/PackageOrderModal.js
@@ -29,19 +29,56 @@ const PackageOrderModal = ({order}) => {
                         <Stage stage={stage} stageRef={1} title="Pagamento 💰">
                             <FormLayout onSubmit={CardSubmit}>
                                 <div className="form-row">
-                                <InputField name="CardNumber" type="text" displayName="Número cartão" placeholder="Número cartão de crédito" className="col-12" /> 
+                                    <InputField name="Cardholder" type="text" displayName="Nome do Titular do Cartão" placeholder="Nome do Titular" required className="col-12"/>
                                 </div>
                                 <div className="form-row">
-                                    <InputField name="CardExpire" type="date" displayName="Vencimento" placeholder="Data de vencimento" className="col-6" /> 
-                                    <InputField name="CardCVV" type="text" displayName="CVV" placeholder="Código de segurança" className="col-6" /> 
+                                <InputField name="CardNumber" type="text" displayName="Número cartão" placeholder="Número cartão de crédito" mask="9999 9999 9999 9999" required className="col-12" /> 
+                                </div>
+                                <div className="form-row">
+                                    <InputField name="CardExpire" type="date" displayName="Vencimento" placeholder="Data de vencimento" required className="col-6" /> 
+                                    <InputField name="CardCVV" type="text" displayName="CVV" placeholder="Código de segurança" mask="999" required className="col-6" /> 
                                 </div>
                             </FormLayout>
                         </Stage>
                         <Stage stage={stage} stageRef={2} title="Sessão 📷">
                             <FormLayout>
+                                <div className="form-row">
+                                    <InputField name="Address" type="text" displayName="Endereço da Sessão" placeholder="Rua Genérica, 123" required className="col-12"/>
+                                </div>
+                                <div className="form-row">
+                                    <InputField name="StartTime" type="datetime-local" displayName="Data/Horário de Início" placeholder="Insira a Data e Horário" required className="col-6"/>
+                                    <InputField name="EndTime" type="datetime-local" displayName="Data/Horário de Fim" placeholder="Insira a Data e Horário" required className="col-6"/>
+                                </div>
                             </FormLayout>
                         </Stage>
                         <Stage stage={stage} stageRef={3} title="Resumo 📃">
+                            <div className="container" className="p-3">
+                                <h5 className="font-weight-bold text-uppercase">Nome do Pacote </h5> 
+                                <h6 className="font-weight-bold mb-4">X Imagens</h6>
+                                <div className="row mb-3">
+                                    <div className="col-12">
+                                        <h6 className="font-weight-bold text-uppercase">Endereço</h6>
+                                        <h6>Endereço da Sessão</h6>
+                                    </div>
+                                </div>
+                                <div className="row mb-3">
+                                    <div className="col-6">
+                                        <h6 className="font-weight-bold text-uppercase">Data/Horário de início</h6>
+                                        <h6>Data do início</h6>
+                                    </div>
+                                    <div className="col-6">
+                                        <h6 className="font-weight-bold text-uppercase">Data/Horário de fim</h6>
+                                        <h6>Data do fim</h6>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <h6 className="font-weight-bold text-uppercase">Valor Final</h6>
+                                        <h6>Valor</h6>
+                                    </div>
+                                </div>
+                                <button type="submit" className="btn btn-outline-primary float-right">Finalizar</button>
+                            </div>
                         </Stage>                   
                     </div>
                 </div>
