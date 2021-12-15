10574d79febf5b4dec867439d8c0e726bf2fad3c
tamaxx <pedro.tamas@uol.com.br>
2021-12-14 21:35:35 -0300

    Info Update

--- src/components/business/TERMS.js
+++ src/components/business/TERMS.js
@@ -6,17 +6,94 @@ const TERMS = () => {
         <div className="row">
             <div className="col-12 col-lg-8 d-flex">
                 <div className="mb-5 mb-lg-auto">
-                    <p>A <i> Out Of Lens </i>, através das condições e termos impostos nessa área, possibilita o acesso organizado e claro do usuário sobre o sistema.</p>
-                    <p>Por favor, leia com atenção as condições descritas abaixo para que a fluidez da navegação ocorra da melhor possível e que potenciais desentendimentos sejam evitados. A utilização do site e contratação de seus serviços presumem a consciência do utilizador sobre os Termos de Uso aqui descritos e a consequente permissão de suas cláusulas e condições.</p>
-                    <h5 className="font-weight-bold">1. Dos Serviços</h5>
-                    <p>1.1 A Out Of Lens, um estúdio independente de fotografia, disponibiliza uma plataforma online no formato de e-commerce para o anúncio e compra de pacotes de fotografia entre empresa e usuário. Além disso, também há a possibilidade de administração por parte dos funcionários e organização do ambiente de trabalho pelos fotógrafos.</p>
-                    <p style={{marginTop: "-10px"}}>1.1.1 As compras são majoritariamente intermediadas pelo site, onde os produtos são comprados, com seus preços acertados e especificações definidas pelo próprio usuário. A partir do momento de compra, os meios de contato com os fotógrafos responsáveis pelo pacote serão disponibilizados ao cliente, que deve ter a liberdade de estabelecer diálogo profissional e esclarecedor com seus prestadores de serviço.</p>
+                    <p>Os serviços da Out Of Lens são fornecidos pela pessoa jurídica OU física com a seguinte Razão Social/nome: Out Of Lens LTDA, com nome fantasia Out Of Lens, inscrito no CNPJ/CPF sob o nº 77.248.557/0001-70, titular da propriedade intelectual sobre software, website, aplicativos, conteúdos e demais ativos relacionados à plataforma da Out Of Lens.</p>
+                    
+                    <h5 className="font-weight-bold">1. Do Objeto</h5>
+                    <p>A plataforma visa licenciar o uso de seu software, website, aplicativos e demais ativos de propriedade intelectual, fornecendo ferramentas para auxiliar e dinamizar o dia a dia dos seus usuários.</p>
+                    <p style={{marginTop: "-10px"}}>A plataforma caracteriza-se pela prestação do seguinte serviço: E-commerce e administração de um estúdio de fotografia.</p>
+                    <p style={{marginTop: "-10px"}}>A plataforma realiza a venda à distância por meio eletrônico dos seguintes produtos ou serviços: Pacotes de ensaios fotográficos.</p>
                 </div>
             </div>
             <div className="col-12 col-lg-4 d-flex justify-content-center">
                 <img src={personal} className="w-75" alt="That's important stuff!!"/>
             </div>
         </div>
+
+        <h5 className="font-weight-bold">2. Da Aceitação</h5>
+        <p>O presente Termo estabelece obrigações contratadas de livre e espontânea vontade, por tempo indeterminado, entre a plataforma e as pessoas físicas ou jurídicas, usuárias do OU site OU aplicativo.</p>
+        <p style={{marginTop: "-10px"}}>Ao utilizar a plataforma o usuário aceita integralmente as presentes normas e compromete-se a observá-las, sob o risco de aplicação das penalidade cabíveis.</p>
+        <p style={{marginTop: "-10px"}}>A aceitação do presente instrumento é imprescindível para o acesso e para a utilização de quaisquer serviços fornecidos pela empresa. Caso não concorde com as disposições deste instrumento, o usuário não deve utilizá-los.</p>
+
+        <h5 className="font-weight-bold">3. Do Acesso dos usuários</h5>
+        <p>Serão utilizadas todas as soluções técnicas à disposição do responsável pela plataforma para permitir o acesso ao serviço 24 (vinte e quatro) horas por dia, 7 (sete) dias por semana. No entanto, a navegação na plataforma ou em alguma de suas páginas poderá ser interrompida, limitada ou suspensa para atualizações, modificações ou qualquer ação necessária ao seu bom funcionamento.</p>
+        
+        <h5 className="font-weight-bold">4. Do Cadastro</h5>
+        <p>O acesso às funcionalidades da plataforma exigirá a realização de um cadastro prévio e, a depender dos serviços ou produtos escolhidos, o pagamento de determinado valor.</p>
+        <p style={{marginTop: "-10px"}}>Ao se cadastrar o usuário deverá informar dados completos, recentes e válidos, sendo de sua exclusiva responsabilidade manter referidos dados atualizados, bem como o usuário se compromete com a veracidade dos dados fornecidos.</p>
+        <p style={{marginTop: "-10px"}}>O usuário se compromete a não informar seus dados cadastrais e/ou de acesso à plataforma a terceiros, responsabilizando-se integralmente pelo uso que deles seja feito.</p>
+        <p style={{marginTop: "-10px"}}>Menores de 18 anos e aqueles que não possuírem plena capacidade civil deverão obter previamente o consentimento expresso de seus responsáveis legais para utilização da plataforma e dos serviços ou produtos, sendo de responsabilidade exclusiva dos mesmos o eventual acesso por menores de idade e por aqueles que não possuem plena capacidade civil sem a prévia autorização.</p>
+        <p style={{marginTop: "-10px"}}>Mediante a realização do cadastro o usuário declara e garante expressamente ser plenamente capaz, podendo exercer e usufruir livremente dos serviços e produtos.</p>
+        <p style={{marginTop: "-10px"}}>O usuário deverá fornecer um endereço de e-mail válido, através do qual o site realizará todas comunicações necessárias.</p>
+        <p style={{marginTop: "-10px"}}>Após a confirmação do cadastro, o usuário possuirá um login e uma senha pessoal, a qual assegura ao usuário o acesso individual à mesma. Desta forma, compete ao usuário exclusivamente a manutenção de referida senha de maneira confidencial e segura, evitando o acesso indevido às informações pessoais.</p>
+        <p style={{marginTop: "-10px"}}>Toda e qualquer atividade realizada com o uso da senha será de responsabilidade do usuário, que deverá informar prontamente a plataforma em caso de uso indevido da respectiva senha.</p>
+        <p style={{marginTop: "-10px"}}>Não será permitido ceder, vender, alugar ou transferir, de qualquer forma, a conta, que é pessoal e intransferível.</p>
+        <p style={{marginTop: "-10px"}}>Caberá ao usuário assegurar que o seu equipamento seja compatível com as características técnicas que viabilize a utilização da plataforma e dos serviços ou produtos.</p>
+        <p style={{marginTop: "-10px"}}>O usuário poderá, a qualquer tempo, requerer o cancelamento de seu cadastro junto ao site Out Of Lens OU aplicativo Out Of Lens. O seu descadastramento será realizado o mais rapidamente possível, desde que não sejam verificados débitos em aberto.</p>
+        <p style={{marginTop: "-10px"}}>O usuário, ao aceitar os Termos e Política de Privacidade, autoriza expressamente a plataforma a coletar, usar, armazenar, tratar, ceder ou utilizar as informações derivadas do uso dos serviços, do site e quaisquer plataformas, incluindo todas as informações preenchidas pelo usuário no momento em que realizar ou atualizar seu cadastro, além de outras expressamente descritas na Política de Privacidade que deverá ser autorizada pelo usuário.</p>
+
+        <h5 className="font-weight-bold">5. Dos serviços ou produtos</h5>
+        <p>A plataforma poderá disponibilizar para o usuário um conjunto específico de funcionalidades e ferramentas para otimizar o uso dos serviços e produtos.</p>
+        <p style={{marginTop: "-10px"}}>Na plataforma os serviços ou produtos oferecidos estão descritos e apresentados com o maior grau de exatidão, contendo informações sobre suas características, qualidades, quantidades, composição, preço, garantia, prazos de validade e origem, entre outros dados, bem como sobre os riscos que apresentam à saúde e segurança do usuário.</p>
+        <p style={{marginTop: "-10px"}}>Antes de finalizar a compra sobre determinado produto ou serviço, o usuário deverá se informar sobre as suas especificações e sobre a sua destinação.</p>
+
+        <h5 className="font-weight-bold">6. Dos preços</h5>
+        <p>A plataforma se reserva no direito de reajustar unilateralmente, a qualquer tempo, os valores dos serviços ou produtos sem consulta ou anuência prévia do usuário.</p>
+        <p style={{marginTop: "-10px"}}>Os valores aplicadas são aqueles que estão em vigor no momento do pedido.</p>
+        <p style={{marginTop: "-10px"}}>Os preços são indicados em reais e não incluem as taxas de entrega, as quais são especificadas à parte e são informadas ao usuário antes da finalização do pedido.</p>
+        <p style={{marginTop: "-10px"}}>Na contratação de determinado serviço ou produto, a plataforma poderá solicitar as informações financeiras do usuário, como CPF, endereço de cobrança e dados de cartões. Ao inserir referidos dados o usuário concorda que sejam cobrados, de acordo com a forma de pagamento que venha a ser escolhida, os preços então vigentes e informados quando da contratação. Referidos dados financeiros poderão ser armazenadas para facilitar acessos e contratações futuras.</p>
+
+        <h5 className="font-weight-bold">7. Do suporte</h5>
+        <p>Em caso de qualquer dúvida, sugestão ou problema com a utilização da plataforma, o usuário poderá entrar em contato com o suporte, através do email widestview@gmail.com OU telefone (11) 28366-4540.</p>
+
+        <h5 className="font-weight-bold">8. Das responsabilidades</h5>
+        <p className="font-weight-bold">É de responsabilidade do usuário:</p>
+        <p style={{marginTop: "-10px"}}>a) defeitos ou vícios técnicos originados no próprio sistema do usuário;</p>
+        <p style={{marginTop: "-10px"}}>b) a correta utilização da plataforma, dos serviços ou produtos oferecidos, prezando pela boa convivência, pelo respeito e cordialidade entre os usuários;</p>
+        <p style={{marginTop: "-10px"}}>c) pelo cumprimento e respeito ao conjunto de regras disposto nesse Termo de Condições Geral de Uso, na respectiva Política de Privacidade e na legislação nacional e internacional;</p>
+        <p style={{marginTop: "-10px"}}>d) pela proteção aos dados de acesso à sua conta/perfil (login e senha).</p>
+        <p className="font-weight-bold" style={{marginTop: "-10px"}}>É de responsabilidade da plataforma: Out Of Lens</p>
+        <p style={{marginTop: "-10px"}}>a) indicar as características do serviço ou produto;</p>
+        <p style={{marginTop: "-10px"}}>b) os defeitos e vícios encontrados no serviço ou produto oferecido desde que lhe tenha dado causa;</p>
+        <p style={{marginTop: "-10px"}}>c) as informações que foram por ele divulgadas, sendo que os comentários ou informações divulgadas por usuários são de inteira responsabilidade dos próprios usuários;</p>
+        <p style={{marginTop: "-10px"}}>d) os conteúdos ou atividades ilícitas praticadas através da sua plataforma.</p>
+        <p style={{marginTop: "-10px"}}>A plataforma não se responsabiliza por links externos contidos em seu sistema que possam redirecionar o usuário à ambiente externo a sua rede.</p>
+        <p style={{marginTop: "-10px"}}>Não poderão ser incluídos links externos ou páginas que sirvam para fins comerciais ou publicitários ou quaisquer informações ilícitas, violentas, polêmicas, pornográficas, xenofóbicas, discriminatórias ou ofensivas.</p>
+
+        <h5 className="font-weight-bold">9. Dos direitos autorais</h5>
+        <p>O presente Termo de Uso concede aos usuários uma licença não exclusiva, não transferível e não sublicenciável, para acessar e fazer uso da plataforma e dos serviços e produtos por ela disponibilizados.</p>
+        <p style={{marginTop: "-10px"}}>A estrutura do site ou aplicativo, as marcas, logotipos, nomes comerciais, layouts, gráficos e design de interface, imagens, ilustrações, fotografias, apresentações, vídeos, conteúdos escritos e de som e áudio, programas de computador, banco de dados, arquivos de transmissão e quaisquer outras informações e direitos de propriedade intelectual da razão social Out Of Lens LTDA, observados os termos da Lei da Propriedade Industrial (Lei nº 9.279/96), Lei de Direitos Autorais (Lei nº 9.610/98) e Lei do Software (Lei nº 9.609/98), estão devidamente reservados.</p>
+        <p style={{marginTop: "-10px"}}>Este Termos de Uso não cede ou transfere ao usuário qualquer direito, de modo que o acesso não gera qualquer direito de propriedade intelectual ao usuário, exceto pela licença limitada ora concedida.</p>
+        <p style={{marginTop: "-10px"}}>O uso da plataforma pelo usuário é pessoal, individual e intransferível, sendo vedado qualquer uso não autorizado, comercial ou não-comercial. Tais usos consistirão em violação dos direitos de propriedade intelectual da razão social Out Of Lens LTDA, puníveis nos termos da legislação aplicável.</p>
+
+        <h5 className="font-weight-bold">10. Das sanções</h5>
+        <p>Sem prejuízo das demais medidas legais cabíveis, a razão social Out Of Lens LTDA poderá, a qualquer momento, advertir, suspender ou cancelar a conta do usuário:</p>
+        <p style={{marginTop: "-10px"}}>a) que violar qualquer dispositivo do presente Termo;</p>
+        <p style={{marginTop: "-10px"}}>b) que descumprir os seus deveres de usuário;</p>
+        <p style={{marginTop: "-10px"}}>c) que tiver qualquer comportamento fraudulento, doloso ou que ofenda a terceiros.</p>
+        
+        <h5 className="font-weight-bold">11. Da recisão</h5>
+        <p>A não observância das obrigações pactuadas neste Termo de Uso ou da legislação aplicável poderá, sem prévio aviso, ensejar a imediata rescisão unilateral por parte da razão social Out Of Lens LTDA e o bloqueio de todos os serviços prestados ao usuário.</p>
+
+        <h5 className="font-weight-bold">12. Das alterações</h5>
+        <p>Os itens descritos no presente instrumento poderão sofrer alterações, unilateralmente e a qualquer tempo, por parte da Out Of Lens, para adequar ou modificar os serviços, bem como para atender novas exigências legais. As alterações serão veiculadas OU pelo site Out Of Lens OU pelo aplicativo Out Of Lens e o usuário poderá optar por aceitar o novo conteúdo ou por cancelar o uso dos serviços, caso seja assinante de algum serviço.</p>
+
+        <h5 className="font-weight-bold">13. Da política de privacidade</h5>
+        <p>Além do presente Termo, o usuário deverá consentir com as disposições contidas na respectiva Política de Privacidade a ser apresentada a todos os interessados dentro da interface da plataforma.</p>
+
+        <h5 className="font-weight-bold">14. Do foro</h5>
+        <p className="font-weight-bold">Para a solução de controvérsias decorrentes do presente instrumento será aplicado integralmente o Direito brasileiro.</p>
+        <p className="font-weight-bold">Os eventuais litígios deverão ser apresentados no foro da comarca em que se encontra a sede da empresa.</p>
+
     </div> );
 }
  
